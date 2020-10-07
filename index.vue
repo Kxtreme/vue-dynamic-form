71d670d8fce147bcc9c2a2a421a7302e7ad638fc
Tiago <tiagodabatalha@hotmail.com>
2020-10-07 23:13:07 +0100

    first commit

---
+++ index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div v-for="result in results"  :key=result">
+
+    </div>
+
+</template>
+<script>
+    export default {
+        props: {
+            result: {
+                type: Array,
+                required: true
+            }
+        },
+        methods: {
+
+        }
+    }
+</script>
