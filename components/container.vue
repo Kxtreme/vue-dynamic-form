83f5abb7db4dc96e3da5697224406c022ea52c51
Tiago <tiagodabatalha@hotmail.com>
2020-10-08 22:42:58 +0100

    added input text

--- components/container.vue
+++ components/container.vue
@@ -3,9 +3,11 @@
         <div class="alert alert-info">
             <div v-for="c in content" :key="c.id">
                 <container v-if="c.type=='container'" :content="c.content" @selfDestroy="deleteFromContainer(c)"/>
+                <text-input v-else-if="c.type=='textInput'" v-model="c.name" @selfDestroy="deleteFromContainer(c)"/>
             </div>
             <span class="left-icons">
                 <a class="btn btn-default" @click="addContainer"><folder-plus-outline /></a>
+                <a class="btn btn-default" @click="addTextInput"><text-box-plus-outline /></a>
             </span>
             <span class="right-icons">
                 <a v-if="!isRoot" class="btn btn-light" @click="$emit('selfDestroy')"><delete class="light-button-content" /></a>
@@ -17,7 +19,9 @@
 <script>
 import '../styles.css';
 import FolderPlusOutline from 'vue-material-design-icons/FolderPlusOutline.vue';
+import TextBoxPlusOutline from 'vue-material-design-icons/TextBoxPlusOutline.vue';
 import Delete from 'vue-material-design-icons/Delete.vue';
+import TextInput from './textInput.vue';
 export default {
     props: {
         content: {
@@ -40,6 +44,16 @@ export default {
                 id: this.generateRandomId()
             })
         },
+
+        addTextInput() {
+            this.content.push({
+                type: 'textInput',
+                content: {
+                    name:''
+                },
+                id: this.generateRandomId()
+            })
+        },
         deleteFromContainer(item) {
             if( item.type == 'container' &&
             item.content.length != 0 && !confirm("Are you sure?")) {
@@ -51,8 +65,10 @@ export default {
     },
     components: {
         FolderPlusOutline,
+        TextBoxPlusOutline,
         Delete,
-        container: () => import('./container')
+        container: () => import('./container'),
+        TextInput
     }
 }
 </script>
---
+++ components/textInput.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <input class="form-control" v-model="name" @input="updateName()">
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                name: ''
+            };
+        },
+        methods: {
+            updateName(){
+                this.$emit('input', this.name)
+            }
+        }
+    }
+</script>
