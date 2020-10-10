5a216bcc02647ddbaf23a515f85eecd0df3200f3
Tiago <tiagodabatalha@hotmail.com>
2020-10-10 14:38:27 +0100

    adapted text input style

--- components/textInput.vue
+++ components/textInput.vue
@@ -1,11 +1,12 @@
 <template>
-    <div>
-        <input class="form-control col-11 align-left" v-model="name" @input="updateName()">
+    <div class="input-text-container">
+        <input class="form-control col-11 align-left" v-model="name" @input="updateName()" placeholder="Field name">
         <a class="btn align-right" @click="$emit('selfDestroy')"><minus-circle-outline /></a>
     </div>
 </template>
 
 <script>
+import '../styles.css';
 import MinusCircleOutline from 'vue-material-design-icons/MinusCircleOutline.vue';
 
     export default {
